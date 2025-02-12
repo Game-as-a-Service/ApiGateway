15bd6829cac2c791389b45a14167b1c36e4c6382
Rick Su <email@rick.su>
2025-02-12 22:01:17 +1100

    feat: add x-forwarded-for-*

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.All;
+});
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 app.MapReverseProxy();
 
